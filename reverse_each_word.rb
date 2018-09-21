e913530296f09f78ebb9d0700134a35e03e57bea
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-09-21 11:03:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   blank.join(' ')
 end
+
+def reverse_each_word(string)
+  blank = []
+  array = string.split()
+  array.collect do |word|
+  blank.push(word.reverse) 
+  end
+  blank.join(' ')
+end
